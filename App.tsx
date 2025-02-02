9fd01a03ff566348a0e085bbdf3fae2cd6d3de16
Iuri Reis <123806396+ithauront@users.noreply.github.com>
2025-02-02 16:49:01 +0100

    [feat]: fonts and loading component

--- App.tsx
+++ App.tsx
@@ -1,14 +1,20 @@
 import { Groups } from '@screens/Groups';
-import { StatusBar } from 'react-native';
+import { StatusBar,} from 'react-native';
 import { ThemeProvider } from 'styled-components/native';
 import theme from './src/theme';
+import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
+import { Loading } from '@components/Loading';
 
 
 export default function App() {
+ const [fontsLoaded] = useFonts({
+    Roboto_400Regular, Roboto_700Bold
+  })
+
   return (
     <ThemeProvider theme={theme} >
        <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
-      <Groups />
+    { fontsLoaded ? <Groups /> : <Loading /> }
     </ThemeProvider>
   );
 }
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,11 @@
+import { Text } from "react-native";
 import { Container} from "./styles";
 
 
 export function Groups(){
     return(
         <Container>
-          
+         
         </Container>
     )
 }
