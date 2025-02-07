434553a36671a5c9a0ddc53aaeb50ce1de612b8e
Iuri Reis <123806396+ithauront@users.noreply.github.com>
2025-02-07 09:27:20 +0100

    [feat]: components - player card

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,10 +1,21 @@
 import { Header } from "@components/Header";
-import { Container, Form } from "./styles";
+import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
 import { Highlight } from "@components/Highlight";
 import { ButtonIcon } from "@components/ButtonIcon";
 import { Input } from "@components/Input";
+import { Filter } from "@components/Filter";
+import { FlatList } from "react-native";
+import { useState } from "react";
+import { PlayerCard } from "@components/PlayerCard";
+import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
 
 export function Players() {
+
+    const [team, setTeam] = useState('Time A')
+    const [players, setPlayers] = useState(['Iuri', 'Queise'])
+
+ 
     return(
         <Container >
             <Header showBackButton/>
@@ -18,7 +29,37 @@ export function Players() {
               />
               <ButtonIcon icon="add"/>
             </Form>
-          
+            <HeaderList>
+            <FlatList
+                data={['Time A', 'Time B']}
+                keyExtractor={(item)=>item}
+                renderItem={({item})=>(
+                    <Filter title={item} isActive={item === team}
+                    onPress={()=>setTeam(item)}
+                    />
+                )}
+            horizontal
+            />
+            <NumberOfPlayers>{players.length}</NumberOfPlayers>
+            </HeaderList>
+                <FlatList
+                    data={players}
+                    keyExtractor={(item)=>item}
+                    renderItem={({item})=>(
+                        <PlayerCard name={item} onRemove={()=>console.log('clicou em remover')}/>
+                    )}
+                 
+                    ListEmptyComponent={()=>(
+                        <ListEmpty message="Não ha pessoas nesse time." />
+                    )}
+                    showsVerticalScrollIndicator={false}
+                    contentContainerStyle={[
+                        {paddingBottom: 100}, 
+                        players.length === 0 && {flex: 1}
+                    ]}
+                />
+         
+          <Button title="Remover turma" type="SECONDARY"/>
         </Container>
     )
 }
